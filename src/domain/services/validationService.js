d4e4f24e5aec01794af6499cbb07d305c0fcb832
JefersonBC <eusouojel@gmail.com>
2024-09-14 18:30:10 -0300

    Remove unnecessary validations

--- src/domain/services/validationService.js
+++ src/domain/services/validationService.js
@@ -1,13 +1,5 @@
-export const validateTransactionInput = (account, totalAmount, mcc) => {
+export const validateTransactionInput = (mcc) => {
   const validMccs = ['5411', '5412', '5811', '5812'];
-
-  if (!account) {
-    return { isValid: false, error: 'Account not found' };
-  }
-
-  if (typeof totalAmount !== 'number' || totalAmount <= 0) {
-    return { isValid: false, error: 'Invalid totalAmount. It must be a positive number' };
-  }
 
   if (!validMccs.includes(mcc)) {
     return { isValid: false, error: 'Invalid MCC code.' };
