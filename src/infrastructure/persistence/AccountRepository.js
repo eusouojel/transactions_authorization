790bccf9e4c61c2b9d54f3cffe40cede453c173e
JefersonBC <eusouojel@gmail.com>
2024-09-14 01:29:34 -0300

    Add Account and Transaction repositories

---
+++ src/infrastructure/persistence/AccountRepository.js
@@ -0,0 +1,28 @@
+import knex from 'knex';
+import knexConfig from '../../../knexfile.cjs';
+import { Account } from '../../domain/entities/account.js';
+
+const db = knex(knexConfig.development);
+
+export const findAccountById = async accountId => {
+  const accountRecord = await db('accounts').where({ id: accountId }).first();
+  
+  if (!accountRecord) {
+    return null;
+  }
+
+  return Account(
+    accountRecord.id,
+    accountRecord.food_balance,
+    accountRecord.meal_balance,
+    accountRecord.cash_balance
+  );
+};
+
+export const updateAccount = async account => {
+  await db('accounts').where({ id: account.id }).update({
+    food_balance: account.foodBalance,
+    meal_balance: account.mealBalance,
+    cash_balance: account.cashBalance
+  });
+};
