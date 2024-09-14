46c4403750ead813885a3fa387bf964128fae1e0
JefersonBC <eusouojel@gmail.com>
2024-09-14 18:29:07 -0300

    Add express validator input validation

--- src/interfaces/http/transactionsController.js
+++ src/interfaces/http/transactionsController.js
@@ -1,22 +1,39 @@
 import express from 'express';
+import { body, validationResult } from 'express-validator';
 import { authorizeTransactionUseCase } from '../../application/usecases/authorizeTransactionUseCase.js';
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-  const { accountId, totalAmount, mcc, merchant } = req.body;
-  
-  try {
-    const result = await authorizeTransactionUseCase(accountId, totalAmount, mcc, merchant);
+router.post(
+  '/',
+  [
+    body('accountId').isInt().notEmpty(),
+    body('totalAmount').isFloat({ gt: 0 }),
+    body('mcc').isString().notEmpty(),
+    body('merchant').isString().notEmpty(),
+  ],
+  async (req, res) => {
 
-    if (!result.success) {
-      return res.status(400).json({ error: result.error });
+    const inputValidationErrors = validationResult(req);
+    
+    if (!inputValidationErrors.isEmpty()) {
+      return res.status(200).json({ code: '07', errors: inputValidationErrors.array() });
     }
+    
+    const { accountId, totalAmount, mcc, merchant } = req.body;
+    
+    try {
+      const result = await authorizeTransactionUseCase(accountId, totalAmount, mcc, merchant);
 
-    return res.status(200).json({ code: result.code });
-  } catch (error) {
-    return res.status(200).json({ error: `Internal Server Error - ${error}` });
+      if (!result.success) {
+        return res.status(200).json({ error: result.code });
+      }
+
+      return res.status(200).json({ code: result.code });
+    } catch (error) {
+      return res.status(200).json({ error: `Internal Server Error - ${error}` });
+    }
   }
-});
+);
 
 export default router;
