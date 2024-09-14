5afbbd0264a7e2ad8ec831bfb02f9dd98aece855
JefersonBC <eusouojel@gmail.com>
2024-09-14 18:28:31 -0300

    Fix typo

--- src/domain/entities/transaction.js
+++ src/domain/entities/transaction.js
@@ -1,4 +1,4 @@
-export const Account = (id, foodBalance, mealBalance, cashBalance) => ({
+export const Transaction = (id, foodBalance, mealBalance, cashBalance) => ({
   accountId,
   totalAmount,
   mcc,
