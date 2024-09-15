0229c41eb4cdeadf0647e4a04db676cebfa0bb33
JefersonBC <eusouojel@gmail.com>
2024-09-14 21:46:52 -0300

    Refact implementation

--- src/interfaces/http/transactionsController.js
+++ src/interfaces/http/transactionsController.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import { body, validationResult } from 'express-validator';
 import { authorizeTransactionUseCase } from '../../application/usecases/authorizeTransactionUseCase.js';
+import { AccountRepositoryDatabase } from '../../infrastructure/persistence/repositories/AccountRepositoryDatabase.js';
+import { TransactionRepositoryDatabase } from '../../infrastructure/persistence/repositories/TransactionRepositoryDatabase.js';
 
 const router = express.Router();
+
+const accountRepository = AccountRepositoryDatabase();
+const transactionRepository = TransactionRepositoryDatabase();
+
+const authorizeTransaction = authorizeTransactionUseCase({
+  accountRepository,
+  transactionRepository,
+});
 
 router.post(
   '/',
@@ -13,25 +23,25 @@ router.post(
     body('merchant').isString().notEmpty(),
   ],
   async (req, res) => {
-
-    const inputValidationErrors = validationResult(req);
-    
-    if (!inputValidationErrors.isEmpty()) {
-      return res.status(200).json({ code: '07', errors: inputValidationErrors.array() });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(200).json({ code: '07', errors: errors.array() });
     }
-    
-    const { accountId, totalAmount, mcc, merchant } = req.body;
-    
-    try {
-      const result = await authorizeTransactionUseCase(accountId, totalAmount, mcc, merchant);
 
-      if (!result.success) {
-        return res.status(200).json({ error: result.code });
-      }
+    const { accountId, totalAmount, mcc, merchant } = req.body;
+
+    try {
+      const result = await authorizeTransaction({
+        accountId,
+        totalAmount: parseFloat(totalAmount),
+        mcc,
+        merchant,
+      });
 
       return res.status(200).json({ code: result.code });
     } catch (error) {
-      return res.status(200).json({ error: `Internal Server Error - ${error}` });
+      console.error('Erro interno do servidor -', error);
+      return res.status(200).json({ code: '07' });
     }
   }
 );
