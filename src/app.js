57087969b5a52b5161b8844ddc1411952a2a7360
JefersonBC <eusouojel@gmail.com>
2024-09-16 08:28:26 -0300

    Add api version

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import routes from './interfaces/http/routes.js';
+import accountRoutes from './interfaces/http/accountRoutes.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', routes);
+app.use('/api/v1', routes);
+app.use('/api/v1/accounts', accountRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
